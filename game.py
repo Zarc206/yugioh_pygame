5203b0c1fc7af0eb6eb7951640c8d6886902b5de
Leo Rafacz <leorafacz@wirelessprv-10-194-109-195.near.illinois.edu>
2025-09-29 23:56:42 -0500

    started dune

--- game.py
+++ game.py
@@ -1,140 +1,110 @@
 import pygame
-import math
-
+import random
 pygame.init()
 
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((1200,800))
 clock = pygame.time.Clock()
 
-surfaces = []
-buttons = []
+font = pygame.font.Font(None,50)
+
 messages = []
+drawings = []
+buttons = []
 
-keyA = False
-keyD = False
-keyW = False
-keyS = False
+refineries = [(5,5),(13,8),(22,31), (33,13), (20,16), (35,35), (42,20)]
+generators = [(33,20), (31,20),(29,20)]
+landingPads = [(33,24),(33,23),(32,24),(32,23),(30,24),(30,23),(29,23),(29,24)]
+buildings = [(35,18),(34,18),(33,18),(32,18),(31,18),(30,18),(29,18),(28,18),(27,18),(35,19),(35,20),(35,21),(35,22),(35,23),(35,24),(35,25),(35,26),(34,26),(33,26),(32,26),(31,26),(30,26),(29,26),(28,26),(27,26),(27,19),(27,20),(27,21),(27,22),(27,23),(27,24),(27,25),(27,26)]
 
-class SurfaceImage:
-    def __init__(self,image,x,y):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()   
-        self.rect.left = x
-        self.rect.y = y
-        surfaces.append(self)
+selectorCoordinates = [20,20]
+
+class Drawing:
+    def __init__(self,x,y,w,h,img):
+        self.image = pygame.image.load(img)
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.rect = pygame.Rect(x,y,w,h)
+        drawings.append(self)
 
     def draw(self):
-        screen.blit(self.image,self.rect)
-    
-    def upkeep(self):
-        None
+        screen.blit(self.image,(self.x,self.y))
 
-class SurfaceButton(SurfaceImage):
-    def __init__(self,image,x,y,message):
-        super().__init__(image,x,y)
+class ClickDrawing(Drawing):
+    def __init__(self,x,y,w,h,img,message):
+        super().__init__(x,y,w,h,img)
         self.message = message
         buttons.append(self)
+
+
+    def __init__(self,name,img):
+        self.name = name
+        self.image = img
     
-    def press(self):
-        messages.append(self.message)    
-
-class Character(SurfaceImage):
-    def __init__(self,image,x,y):
-        super().__init__(image,x,y)
-        self.image = pygame.transform.scale2x(self.image)
-        self.frameNum = 0
-        self.animationNum = 0
-        self.rect.width = (42) 
-        self.rect.height = 67
-
-
+class Text:
+    def __init__(self,text,color,x,y):
+        self.text = font.render(text,None,color)
+        self.x = x
+        self.y = y
+        drawings.append(self)
+    
     def draw(self):
-        self.rect.width = 42
-        if self.animationNum == 0 or self.animationNum == 3:
-            self.rect.width = 49
-        screen.blit(self.image,self.rect,(math.floor(self.frameNum) * self.rect.width,self.animationNum * self.rect.height,self.rect.width,self.rect.height))
+        screen.blit(self.text,(self.x,self.y))
 
-    def upkeep(self):
-        self.frameNum = 0
+def createMap():
 
+    for ii in range(43):
+        for i in range(53):
+            if (i,ii) in buildings:
+                sand = Drawing(850 + 28 * ii - 28 * i,-700 + 20 * ii + 20 * i,56,74,"assets/building.webp")
+            else:
+                sand = Drawing(850 + 28 * ii - 28 * i,-700 + 20 * ii + 20 * i,56,74,"assets/sand.webp")
+                if (i,ii) in refineries:
+                    refinery = Drawing(850 + 28 * ii - 28 * i + 8,-700 + 20 * ii + 20 * i,49,40,"assets/refinery.webp")
+                if (i,ii) in generators:
+                    generator = Drawing(850 + 28 * ii - 28 * i + 8,-700 + 20 * ii + 20 * i - 10,49,40,"assets/generator.webp")
+                if (i,ii) in landingPads:
+                    landingPad = Drawing(850 + 28 * ii - 28 * i,-700 + 20 * ii + 20 * i,56,74,"assets/landingPad.webp")
 
-class Player(Character):
-    def __init__(self,image,x,y):
-        super().__init__(image,x,y)
-        self.speed = 2
+createMap()
 
-    def draw(self):
-        self.rect.width = 42
-        if self.animationNum == 0 or self.animationNum == 3:
-            self.rect.width = 48
-        screen.blit(self.image,self.rect,(math.floor(self.frameNum) * self.rect.width,self.animationNum * self.rect.height,self.rect.width,self.rect.height))
+selector = Drawing(850 + 28 * 20 - 28 * 20,-700 + 20 * 20 + 20 * 20,56,74,"assets/selector.webp")
 
-    def upkeep(self):
+peopleIndicator = Drawing(200,0,200,50,"assets/peopleIndicator.png")
+peopleText = Text(str(buildings.__len__()) + "/" + str(buildings.__len__()),"white",300,8)
 
-        if keyA:
-            self.animationNum = 2
-            self.rect.left -= self.speed
-        elif keyW:
-            self.animationNum = 0
-            self.rect.top -= self.speed
-        elif keyS:
-            self.animationNum = 3
-            self.rect.top += self.speed
-        elif keyD:
-            self.animationNum = 1
-            self.rect.left += self.speed
-
-
-        if keyA or keyW or keyS or keyD:
-            self.frameNum += 0.1
-        else:
-            self.frameNum = 0
-
-        if self.frameNum >= 3:
-            self.frameNum = 0
-        
-
-bg = SurfaceImage("assets/background.png",0,0)
-startButton = SurfaceButton("assets/startButton.png",100,100,"start")
-
-while True:
-
-    if "start" in messages:
-        messages.remove("start")
-        grid = SurfaceImage("assets/grid.png",50,50)
-        yugi = Player('assets/characters/yugi.png',100,100)
-
+running = True
+while running:
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            for button in buttons:
-                if button.rect.collidepoint(pos):
-                    surfaces.remove(button)
-                    buttons.remove(button)
-                    button.press()
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                for button in buttons:
+                    if button.message != None:
+                        if button.rect.collidepoint(event.pos):
+                            messages.append(button.message)
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                keyA = True
-            if event.key == pygame.K_d:
-                keyD = True
-            if event.key == pygame.K_w:
-                keyW = True
-            if event.key == pygame.K_s:
-                keyS = True
+            if event.key == pygame.K_DOWN:
+                selectorCoordinates[1] += 1
+            if event.key == pygame.K_UP:
+                selectorCoordinates[1] -= 1
+            if event.key == pygame.K_LEFT:
+                selectorCoordinates[0] -= 1
+            if event.key == pygame.K_RIGHT:
+                selectorCoordinates[0] += 1
+            selector.x =  850 + 28 * selectorCoordinates[0] - 28 * selectorCoordinates[1]
+            selector.y = -700 + 20 * selectorCoordinates[0] + 20 * selectorCoordinates[1]
+    
+    for drawing in drawings:
+        drawing.draw()
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                keyA = False
-            if event.key == pygame.K_d:
-                keyD = False
-            if event.key == pygame.K_w:
-                keyW = False
-            if event.key == pygame.K_s:
-                keyS = False
 
-    for surface in surfaces:
-        surface.upkeep()
-        surface.draw()
-   
+
+
+
+
     pygame.display.update()
     clock.tick(60)
+
+pygame.quit()
